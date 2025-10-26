6a5020840e4e7281ac7e0d88069c78a0223a2093
Sanan Maarouf <sanan.adnan97@gmail.com>
2025-10-26 22:37:28 +0100

    add internationalized array plugin for multi-language support in Sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { schema } from "./src/sanity/schemaTypes";
+import {internationalizedArray} from 'sanity-plugin-internationalized-array'
+
 export default defineConfig({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID!,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET!,
-  plugins: [structureTool()],
+  plugins: [
+    structureTool(),
+    internationalizedArray({
+      languages: [
+        {id: 'en', title: 'English'},
+        {id: 'nb', title: 'Norwegian Bokmål'}
+      ],
+      defaultLanguages: ['en'],
+      fieldTypes: ['string', 'text', 'blockContent'],
+    })
+  ],
   schema,
 });
